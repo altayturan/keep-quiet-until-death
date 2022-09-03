a0f0223d81d342e785f1e5ee03dfb6e8610729e5
Yaşar Gençer <gncrysr@gmail.com>
2022-09-03 12:43:59 +0300

    Noise Foundation Complete

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,37 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField, Header("0 is default, 1 is sneak, 2 is run")] 
+    private float[] allSpeeds;
+    private int activeSpeed = 0;
     private Transform player;
 
     private bool rightPressed => Input.GetKey(KeyCode.D);
     private bool leftPressed => Input.GetKey(KeyCode.A);
     private bool upPressed => Input.GetKey(KeyCode.W);
     private bool downPressed => Input.GetKey(KeyCode.S);
-
+    private bool runPressed => Input.GetKey(KeyCode.LeftShift);
+    private bool sneakPressed => Input.GetKey(KeyCode.LeftControl);
     void Start()
     {
         player = GetComponent<Transform>();
     }
-
     void Update()
     {
+        MovementSpeed();
         Move();
         Rotate();
     }
-
+    void MovementSpeed()
+    {
+        if (sneakPressed)
+            SetActiveSpeed(1);
+        else if (runPressed)
+            SetActiveSpeed(2);
+        else if (rightPressed || leftPressed || downPressed || upPressed)
+            SetActiveSpeed(0);
+        else
+            SetActiveSpeed(1);
+    }
     void Move()
     {
         if(rightPressed)
-            player.Translate(Vector2.right * speed * Time.deltaTime, Space.World );
+            player.Translate(Vector2.right * allSpeeds[activeSpeed] * Time.deltaTime, Space.World );
         if (leftPressed)
-            player.Translate(Vector2.left * speed * Time.deltaTime, Space.World );
+            player.Translate(Vector2.left * allSpeeds[activeSpeed] * Time.deltaTime, Space.World );
         if (upPressed)
-            player.Translate(Vector2.up * speed * Time.deltaTime,Space.World );
+            player.Translate(Vector2.up * allSpeeds[activeSpeed] * Time.deltaTime,Space.World );
         if (downPressed)
-            player.Translate(Vector2.down * speed * Time.deltaTime, Space.World );
+            player.Translate(Vector2.down * allSpeeds[activeSpeed] * Time.deltaTime, Space.World );
     }
-
     void Rotate()
     {
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -42,4 +54,5 @@ public class PlayerMovement : MonoBehaviour
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         player.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
+    public void SetActiveSpeed(int value) { activeSpeed = value; GetComponent<PlayerNoise>().SetRange(value); }
 }
--- Assets/Scripts/PlayerNoise.cs
+++ Assets/Scripts/PlayerNoise.cs
@@ -1,17 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerNoise : MonoBehaviour
 {
-    private float range = 5;
+    private float range = 0;
 
     [SerializeField, Header("0 is default, 1 is sneak, 2 is run, 3 is fire")]
     private float[] allRanges;
     [SerializeField]
     private LayerMask enemyLayer;
+    private float decreaseCount = .05f;
+    private bool canDecrease = true;
     private void Update()
     {
         SetEnemyNoise();
+        DynamicRange();
     }
+    void DynamicRange()
+    {
+        DecreaseRange();
+    }
+    public void SetRange(float value) { range = value;}
+    public void SetRange(int value)
+    {
+        if (range <= allRanges[value])
+        {
+            SetRange(allRanges[value]);
+            if (canDecrease)
+                StartCoroutine(canDecreaseEnum());
+        }
+    }
+    IEnumerator canDecreaseEnum()
+    {
+        canDecrease = false;
+        yield return new WaitForSeconds(.5f);
+        canDecrease = true;
+    }
+    void DecreaseRange() { if (range >= allRanges[1] && canDecrease) SetRange(range - decreaseCount); }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -22,13 +47,13 @@ public class PlayerNoise : MonoBehaviour
         Gizmos.DrawWireSphere(this.transform.position, allRanges[2]);
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(this.transform.position, allRanges[3]);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, range);
     }
     public void SetEnemyNoise()
     {
         RaycastHit2D[] hits = Physics2D.CircleCastAll(this.transform.position, range, Vector2.zero, 1000, enemyLayer);
         foreach (var rycst in hits)
-        {
             rycst.collider.gameObject.GetComponent<EnemyNavMeshMovement>().SetTargetPos(this.transform.position);
-        }
     }
 }
