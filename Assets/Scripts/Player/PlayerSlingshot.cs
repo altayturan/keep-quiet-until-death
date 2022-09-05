1492f5a27444b7bbe739946c18256b4d7a9653ff
Altay Turan <axtay434@gmail.com>
2022-09-05 18:04:57 +0300

    Something?

--- Assets/Scripts/Player/PlayerSlingshot.cs
+++ Assets/Scripts/Player/PlayerSlingshot.cs
@@ -47,7 +47,6 @@ public class PlayerSlingshot : MonoBehaviour
         hitPointer.GetComponentsInChildren<SpriteRenderer>()[0].color = gradient.Evaluate(rangePercent / 100);
         hitPointer.GetComponentsInChildren<SpriteRenderer>()[1].color = gradient.Evaluate(rangePercent / 100);
 
-        UI.ui.SetSlider(UI.ui.slingSlider, 100, (int)rangePercent);
         hitPointer.transform.position = playerMovement.GetPlayerShootPos().position + this.transform.up * (range * rangePercent / 100);
     }
     public void Shoot()
@@ -55,7 +54,6 @@ public class PlayerSlingshot : MonoBehaviour
         GameObject throws = Instantiate(stone, playerMovement.GetPlayerShootPos().position, Quaternion.identity);
         throws.GetComponent<Slingshot>().SetVariables(speed, range * rangePercent / 100, hitEffect, this.gameObject);
         rangePercent = 0;
-        UI.ui.SetSlider(UI.ui.slingSlider, 100, (int)rangePercent);
         Destroy(hitPointer);
         SetShooting(false); GetComponent<PlayerGunshot>().SetCanTwice(true);
     }
